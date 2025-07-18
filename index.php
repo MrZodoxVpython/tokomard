906273922058d825ac97f8a40e11c5d0e67cfe90
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-07-18 18:58:37 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-07-18 18:58:37

--- index.php
+++ index.php
@@ -29,6 +29,7 @@
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-white text-gray-800">
+
   <!-- Header -->
   <header class="bg-orange-500 text-white">
     <div class="max-w-7xl mx-auto px-4 py-2 flex justify-between items-center text-sm">
