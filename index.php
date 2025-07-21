3ab14bd4368d512c272d4cd276bb65fd64881ba9
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-07-21 19:13:25 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-07-21 19:13:25

--- index.php
+++ index.php
@@ -68,6 +68,7 @@
     <div class="max-w-7xl mx-auto grid grid-cols-2 gap-4 px-4">
       <img src="https://cf.shopee.co.id/file/id-11134258-7ra0j-mbes592yn87z81_xxhdpi" class="w-full rounded-lg" alt="promo 1">
       <img src="https://cf.shopee.co.id/file/id-11134258-7rbk7-m71zp300x3zwda_xhdpi" class="w-full rounded-lg" alt="promo 2">
+      <img src="https://cf.shopee.co.id/file/id-11134258-7rbk7-masco4vng1w296_xhdpi" class="w-full rounded-lg" alt="promo 3">
     </div>
   </div>
 
