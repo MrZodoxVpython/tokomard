85058be3954a378f50051b4e9710014c9efedc36
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-07-21 21:30:28 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-07-21 21:30:28

--- index.php
+++ index.php
@@ -65,7 +65,7 @@
 <!-- Kategori Submenu -->
 <div class="bg-orange-500">
   <div class="max-w-7xl mx-auto px-4 py-2">
-    <div class="flex flex-wrap gap-4 text-white text-sm">
+    <div class="flex flex-wrap justify-center gap-4 text-white text-sm">
       <span class="cursor-pointer hover:underline">Tas</span>
       <span class="cursor-pointer hover:underline">Huawei E537</span>
       <span class="cursor-pointer hover:underline">Antena Huawei E5577</span>
